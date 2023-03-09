64267cdf42a3dcd82fd19f0ced603b76bdd8f32f
🌌 <Time.Rain.Star.Sky@Gmail.com>
2023-03-09 13:24:25 +0800

    优化 Docker 安装脚本

--- Install-Docker.sh
+++ Install-Docker.sh
@@ -1,5 +1,5 @@
 #TRSS Sagiri Docker 安装脚本 作者：时雨🌌星空
-NAME=v1.0.0;VERSION=202303040
+NAME=v1.0.0;VERSION=202303090
 R="[1;31m" G="[1;32m" Y="[1;33m" C="[1;36m" B="[1;m" O="[m"
 echo "$B———————————————————————————
 $R TRSS$Y Sagiri$G Docker$C Script$O
@@ -81,7 +81,8 @@ done
 echo "
 $Y- 正在构建 Docker 容器$O
 "
-mktmp&&cd "$TMP"
+mktmp
+cd "$TMP"
 case "$(uname -m)" in
   aarch64|arm64|armv8*|armv9*)echo 'Server = https://mirrors.ustc.edu.cn/archlinuxarm/$arch/$repo
 Server = https://mirrors.bfsu.edu.cn/archlinuxarm/$arch/$repo
@@ -134,7 +135,8 @@ RUN ln -vsf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime\
 RUN pacman -Syu --noconfirm --needed --overwrite "*" jre-openjdk\
  && rm -rf /var/cache
 RUN echo -n '\''exec bash /root/TRSS_Sagiri/Main.sh "$@"'\''>/usr/local/bin/tssi\
- && chmod 755 /usr/local/bin/tssi'>Dockerfile
+ && chmod 755 /usr/local/bin/tssi
+WORKDIR /root/TRSS_Sagiri'>Dockerfile
 docker build -t trss:sagiri .||abort "Docker 容器构建失败"
 echo "
 $Y- 正在启动 Docker 容器$O
@@ -142,7 +144,9 @@ $Y- 正在启动 Docker 容器$O
 docker rm -f $DKNAME 2>/dev/null
 docker image prune -f
 docker run -itd -h TRSS-Sagiri --name $DKNAME -v "$DIR":/root/TRSS_Sagiri --restart always trss:sagiri||abort "Docker 容器启动失败"
-mkdir -vp "$CMDPATH"&&echo -n "exec docker exec -it $DKNAME bash /root/TRSS_Sagiri/Main.sh "'"$@"'>"$CMDPATH/$CMD"&&chmod 755 "$CMDPATH/$CMD"||abort "脚本执行命令 $CMDPATH/$CMD 设置失败，手动执行命令：docker exec -it $DKNAME bash /root/TRSS_Sagiri/Main.sh"
+mkdir -vp "$CMDPATH"&&
+echo -n "exec docker exec -it $DKNAME bash Main.sh "'"$@"'>"$CMDPATH/$CMD"&&
+chmod 755 "$CMDPATH/$CMD"||abort "脚本执行命令 $CMDPATH/$CMD 设置失败，手动执行命令：docker exec -it $DKNAME bash Main.sh"
 echo "
 $G- Docker 容器安装完成，启动命令：$C$CMD$O";exit;}
 echo "
